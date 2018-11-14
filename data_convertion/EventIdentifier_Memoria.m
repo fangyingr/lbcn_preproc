fac9b507c79a4f27861086c20b1aba58c7712bf7
Ying Fang <ying.fang@outlook.com>
2018-11-13 16:43:53 -0800

    made an exception for S128

--- data_convertion/EventIdentifier_Memoria.m
+++ data_convertion/EventIdentifier_Memoria.m
@@ -8,7 +8,7 @@ switch project_name
         n_stim_per_trial = 5;
     case 'Calculia_production'
         n_stim_per_trial = 3;
-%         n_initpulse = 0; % maybe change to 12
+        %         n_initpulse = 0; % maybe change to 12
 end
 
 %% loop across blocks
@@ -20,19 +20,25 @@ for i = 1:length(block_names)
     load(sprintf('%s/originalData/%s/global_%s_%s_%s.mat',dirs.data_root,sbj_name,project_name,sbj_name,bn));
     iEEG_rate=globalVar.iEEG_rate;
     
-
+    
     %% reading analog channel from neuralData directory
-    load(sprintf('%s/Pdio%s_01.mat',globalVar.originalData,bn)); % going to be present in the globalVar
-     
-   
+    
+    if strcmp(sbj_name,'S18_128_CG')
+        load(sprintf('%s/Pdio%s_03.mat',globalVar.originalData,bn));
+        n_initpulse = 0;
+    else
+        load(sprintf('%s/Pdio%s_01.mat',globalVar.originalData,bn)); % going to be present in the globalVar
+    end
+    
+    
     %% varout is anlg (single percision)
     downRatio= round(globalVar.Pdio_rate/iEEG_rate);
     if max(anlg) < max(abs(anlg))
-       pdio= decimate(double(anlg),downRatio)*-1; % down sample to the iEEG rate and make it positive ?
+        pdio= decimate(double(anlg),downRatio)*-1; % down sample to the iEEG rate and make it positive ?
     else
-       pdio= decimate(double(anlg),downRatio); % down sample to the iEEG rate and make it positive ?
+        pdio= decimate(double(anlg),downRatio); % down sample to the iEEG rate and make it positive ?
     end
-   
+    
     clear anlg
     
     pdio = pdio/max(pdio);
@@ -62,23 +68,23 @@ for i = 1:length(block_names)
     
     %% IF MISMATCH BW PDIO AND BEHAV DATA, EDIT HERE:
     [stim_onset,stim_offset] = StimOnsetExceptions(sbj_name,bn,stim_onset,stim_offset);
-
+    
     stim_dur= stim_offset - stim_onset;
     
-        %% Load trialinfo 
+    %% Load trialinfo
     % ---------------------------------------------
     % Create specific subfunctions to extract the relevant info for each
     % project_name
     load([globalVar.psych_dir '/trialinfo_', bn '.mat'], 'trialinfo');
-
+    
     % Add the all_stim_onset
     event_trials = find(~strcmp(trialinfo.condNames, 'rest'));
-        
+    
     rest_trials = find(strcmp(trialinfo.condNames, 'rest'));
-
+    
     StimulusOnsetTime = trialinfo.StimulusOnsetTime(event_trials,1); % **
-        
-      
+    
+    
     
     %% Get trials, insturuction onsets
     %% modified for Memoria
@@ -104,14 +110,14 @@ for i = 1:length(block_names)
     % the second input is project dependent
     %reshape onsets to account for the number of events in each trial
     
-%%
-   % Plot photodiode segmented data
+    %%
+    % Plot photodiode segmented data
     figureDim = [0 0 1 1];
     figure('units', 'normalized', 'outerposition', figureDim)
     subplot(2,3,1:3)
     hold on
     plot(pdio)
-   
+    
     % plot each stim number in diff. color
     stim_syms = {'m*','r*','g*','c*','b*'};
     heights = 0.9:-0.2:0.1;
@@ -119,9 +125,9 @@ for i = 1:length(block_names)
         stim_inds = find(stimnum_rs==i);
         plot(all_stim_onset_rs(stim_inds)*iEEG_rate,heights(i)*ones(1,length(stim_inds)),stim_syms{i})
     end
-        
     
-
+    
+    
     %% Comparing photodiod with behavioral data
     %for just the first stimulus of each trial
     df_SOT= diff(StimulusOnsetTime)';
@@ -143,7 +149,7 @@ for i = 1:length(block_names)
     title('Diff. behavior diode (hist)');
     xlabel('Time (ms)');
     ylabel('Count');
-
+    
     %flag large difference
     if ~all(abs(df)<.1)
         disp('behavioral data and photodiod mismatch')
@@ -151,9 +157,9 @@ for i = 1:length(block_names)
     end
     
     
-    %% Updating the events with onsets 
+    %% Updating the events with onsets
     trialinfo.allonsets = nan(ntrials,size(all_stim_onset,2));
-    if (size(all_stim_onset,2)>1)      
+    if (size(all_stim_onset,2)>1)
         trialinfo.allonsets(event_trials,:) = all_stim_onset;
     else
         trialinfo.allonsets(event_trials) = all_stim_onset;
@@ -162,13 +168,12 @@ for i = 1:length(block_names)
     for ti = 1:ntrials
         trialinfo.RT_lock(ti) = trialinfo.RT(ti) + trialinfo.allonsets(ti,trialinfo.nstim(ti));
     end
-
     
-    %% Save trialinfo   
-    fn = [dirs.project,filesep,sbj_name,filesep,bn,filesep,'trialinfo_',bn,'.mat'];
-%     fn= sprintf('%s/trialinfo_%s.mat',globalVar.result_dir,bn);
+    
+    %% Save trialinfo
+    disp('updating trialinfo')
+    fn= sprintf('%s/trialinfo_%s.mat',globalVar.psych_dir,bn);
     save(fn, 'trialinfo');
-    
 end
 
 
